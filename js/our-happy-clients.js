33288893a30ba3776d865f7a3989ddc64a3ab00b
Jak2 <jayaarunkumartulluri2@gmail.com>
2024-07-31 08:34:33 +0530

    created website

---
+++ js/our-happy-clients.js
@@ -0,0 +1,21 @@
+let currentSlide = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slider-image');
+    if (index >= slides.length) currentSlide = 0;
+    if (index < 0) currentSlide = slides.length - 1;
+    slides.forEach((slide, i) => {
+        slide.style.transform = `translateX(-${currentSlide * 100}%)`;
+    });
+}
+
+function nextSlide() {
+    showSlide(currentSlide + 1);
+}
+
+function prevSlide() {
+    showSlide(currentSlide - 1);
+}
+
+// Initialize slider
+showSlide(currentSlide);
